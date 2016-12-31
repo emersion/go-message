edb4415358300a638dc6c1661924bd5ddf4395b6
emersion <contact@emersion.fr>
2016-12-31 19:06:57 +0100

    Adds transform example

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/textproto"
+	"strings"
 
 	"github.com/emersion/go-messages"
 )
@@ -62,6 +63,58 @@ func ExampleWriter() {
 	w2.Close()
 
 	w.Close()
+
+	log.Println(b.String())
+}
+
+func Example_transform() {
+	// Let's assume r is an io.Reader that contains a message.
+	var r io.Reader
+
+	m, err := messages.Read(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define a function that transforms entities
+	// For instance, let's add "This message is powered by Go" at the end of each
+	// text entity.
+	poweredBy := "\n\nThis message is powered by Go."
+
+	var transform func(e *messages.Entity) *messages.Entity
+	transform = func(e *messages.Entity) *messages.Entity {
+		if mr := e.MultipartReader(); mr != nil {
+			// This is a multipart entity, transform each of its parts
+			var parts []*messages.Entity
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					log.Fatal(err)
+				}
+
+				p = transform(p)
+				parts = append(parts, p)
+			}
+
+			return messages.NewMultipart(e.Header, parts)
+		} else {
+			if strings.HasPrefix(m.Header.Get("Content-Type"), "text/") {
+				r := io.MultiReader(m.Reader, strings.NewReader(poweredBy))
+				return messages.NewEntity(e.Header, r)
+			}
+			return e
+		}
+	}
+
+	m = transform(m)
+
+	// We can now format the message
+	var b bytes.Buffer
+	if err := m.WriteTo(&b); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println(b.String())
 }
