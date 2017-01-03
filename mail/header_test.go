94a89a4cbef744e12d443a8029e1b2eb3ba4888c
emersion <contact@emersion.fr>
2017-01-03 15:23:34 +0100

    mail: adds tests for Header and Reader

---
+++ mail/header_test.go
@@ -0,0 +1,30 @@
+package mail_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/emersion/go-messages/mail"
+)
+
+func TestHeader(t *testing.T) {
+	date := time.Unix(1466253744, 0)
+	from := []*mail.Address{{"Mitsuha Miyamizu", "mitsuha.miyamizu@example.org"}}
+
+	h := mail.NewHeader()
+	h.SetAddressList("From", from)
+	h.SetDate(date)
+
+	if got, err := h.Date(); err != nil {
+		t.Error("Expected no error while parsing header date, got:", err)
+	} else if !got.Equal(date) {
+		t.Errorf("Expected header date to be %v, but got %v", date, got)
+	}
+
+	if got, err := h.AddressList("From"); err != nil {
+		t.Error("Expected no error while parsing header address list, got:", err)
+	} else if !reflect.DeepEqual(got, from) {
+		t.Errorf("Expected header address list to be %v, but got %v", from, got)
+	}
+}
--- mail/reader_test.go
+++ mail/reader_test.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"strings"
+	"testing"
 
 	"github.com/emersion/go-messages/mail"
 )
@@ -35,5 +37,64 @@ func ExampleReader() {
 			filename, _ := h.Filename()
 			log.Println("Got attachment: %v", filename)
 		}
+	}
+}
+
+func TestReader(t *testing.T) {
+	r := strings.NewReader(mailString)
+	mr, err := mail.CreateReader(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mr.Close()
+
+	i := 0
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		var expectedBody string
+		switch i {
+		case 0:
+			h, ok := p.Header.(mail.TextHeader)
+			if !ok {
+				t.Fatalf("Expected a TextHeader, but got a %T", p.Header)
+			}
+
+			if h.IsHTML() {
+				t.Errorf("Expected a plaintext part, not an HTML part")
+			}
+
+			expectedBody = "Who are you?"
+		case 1:
+			h, ok := p.Header.(mail.AttachmentHeader)
+			if !ok {
+				t.Fatalf("Expected an AttachmentHeader, but got a %T", p.Header)
+			}
+
+			if filename, err := h.Filename(); err != nil {
+				t.Error("Expected no error while parsing filename, but got:", err)
+			} else if filename != "note.txt" {
+				t.Errorf("Expected filename to be %q but got %q", "note.txt", filename)
+			}
+
+			expectedBody = "I'm Mitsuha."
+		}
+
+		if b, err := ioutil.ReadAll(p.Body); err != nil {
+			t.Error("Expected no error while reading part body, but got:", err)
+		} else if string(b) != expectedBody {
+			t.Errorf("Expected part body to be:\n%v\nbut got:\n%v", expectedBody, string(b))
+		}
+
+		i++
+	}
+
+	if i != 2 {
+		t.Errorf("Expected exactly two parts but got %v", i)
 	}
 }
