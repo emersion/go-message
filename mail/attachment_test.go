c151e78bd7011968e79bb518afa72d20970cb5ef
emersion <contact@emersion.fr>
2017-01-03 15:42:36 +0100

    mail: adds test for attachment filename in Content-Type

---
+++ mail/attachment_test.go
@@ -0,0 +1,20 @@
+package mail_test
+
+import (
+	"testing"
+
+	"github.com/emersion/go-messages/mail"
+)
+
+func TestAttachmentHeader_Filename_inContentType(t *testing.T) {
+	// Note: putting the attachment's filename in Content-Type is discouraged.
+
+	h := mail.NewAttachmentHeader()
+	h.Set("Content-Type", "text/plain; name=note.txt")
+
+	if filename, err := h.Filename(); err != nil {
+		t.Error("Expected no error while parsing filename, got:", err)
+	} else if filename != "note.txt" {
+		t.Errorf("Expected filename to be %q but got %q", "note.txt", filename)
+	}
+}
