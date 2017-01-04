df28eaae9b21b8421a74fe3d31e5be860c5b9308
emersion <contact@emersion.fr>
2017-01-04 15:04:11 +0100

    message: adds tests for charset and encoding

---
+++ charset_test.go
@@ -0,0 +1,60 @@
+package message_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/emersion/go-message"
+)
+
+var testCharsets = []struct{
+	charset string
+	encoded []byte
+	decoded string
+}{
+	{
+		charset: "us-ascii",
+		encoded: []byte("yuudachi"),
+		decoded: "yuudachi",
+	},
+	{
+		charset: "utf-8",
+		encoded: []byte("café"),
+		decoded: "café",
+	},
+	{
+		charset: "windows-1252",
+		encoded: []byte{0x63, 0x61, 0x66, 0xE9, 0x20, 0x80},
+		decoded: "café €",
+	},
+	{
+		charset: "iso-8859-1",
+		encoded: []byte{0x63, 0x61, 0x66, 0xE9},
+		decoded: "café",
+	},
+	{
+		charset: "idontexist",
+		encoded: []byte{42},
+	},
+}
+
+func TestCharsetReader(t *testing.T) {
+	for _, test := range testCharsets {
+		r, err := message.CharsetReader(test.charset, bytes.NewReader(test.encoded))
+		if test.decoded == "" {
+			if err == nil {
+				t.Errorf("Expected an error when creating reader for charset %q", test.charset)
+			}
+		}
+		if test.decoded != "" {
+			if err != nil {
+				t.Errorf("Expected no error when creating reader for charset %q, but got: %v", test.charset, err)
+			} else if b, err := ioutil.ReadAll(r); err != nil {
+				t.Errorf("Expected no error when reading charset %q, but got: %v", test.charset, err)
+			} else if s := string(b); s != test.decoded {
+				t.Errorf("Expected decoded text to be %q but got %q", test.decoded, s)
+			}
+		}
+	}
+}
---
+++ encoding_test.go
@@ -0,0 +1,64 @@
+package message
+
+import (
+	"bytes"
+	"strings"
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+var testEncodings = []struct{
+	enc string
+	encoded string
+	decoded string
+}{
+	{
+		enc: "binary",
+		encoded: "café",
+		decoded: "café",
+	},
+	{
+		enc: "8bit",
+		encoded: "café",
+		decoded: "café",
+	},
+	{
+		enc: "7bit",
+		encoded: "hi there",
+		decoded: "hi there",
+	},
+	{
+		enc: "quoted-printable",
+		encoded: "caf=E9",
+		decoded: "café",
+	},
+	{
+		enc: "base64",
+		encoded: "Y2Fmw6k=",
+		decoded: "café",
+	},
+}
+
+func Testdecode(t *testing.T) {
+	for _, test := range testEncodings {
+		r := decode(test.enc, strings.NewReader(test.encoded))
+		if b, err := ioutil.ReadAll(r); err != nil {
+			t.Errorf("Expected no error when reading encoding %q, but got: %v", test.enc, err)
+		} else if s := string(b); s != test.decoded {
+			t.Errorf("Expected decoded text to be %q but got %q", test.decoded, s)
+		}
+	}
+}
+
+func Testencode(t *testing.T) {
+	for _, test := range testEncodings {
+		var b bytes.Buffer
+		wc := encode(test.enc, &b)
+		io.WriteString(wc, test.decoded)
+		wc.Close()
+		if s := b.String(); s != test.encoded {
+			t.Errorf("Expected encoded text to be %q but got %q", test.encoded, s)
+		}
+	}
+}
