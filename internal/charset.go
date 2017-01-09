6d72284b6aa92a3bff98c003454d1516cb52cb30
emersion <contact@emersion.fr>
2017-01-09 17:30:41 +0100

    internal: adds more charsets

--- internal/charset.go
+++ internal/charset.go
@@ -7,10 +7,28 @@ import (
 
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
 )
 
 var charsets = map[string]encoding.Encoding{
+	"big5":         traditionalchinese.Big5,
+	"euc-jp":       japanese.EUCJP,
+	"gb2312":       simplifiedchinese.HZGB2312,
+	"iso-2022-jp":  japanese.ISO2022JP,
 	"iso-8859-1":   charmap.ISO8859_1,
+	"iso-8859-2":   charmap.ISO8859_2,
+	"iso-8859-3":   charmap.ISO8859_3,
+	"iso-8859-4":   charmap.ISO8859_4,
+	"iso-8859-10":  charmap.ISO8859_10,
+	"iso-8859-13":  charmap.ISO8859_13,
+	"iso-8859-14":  charmap.ISO8859_14,
+	"iso-8859-15":  charmap.ISO8859_15,
+	"iso-8859-16":  charmap.ISO8859_16,
+	"koi8-r":       charmap.KOI8R,
+	"shift_jis":    japanese.ShiftJIS,
+	"windows-1251": charmap.Windows1251,
 	"windows-1252": charmap.Windows1252,
 }
 
