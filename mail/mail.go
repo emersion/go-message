9f9a0938b9eac0f0dc6face5f9d831a8465acb0c
emersion <contact@emersion.fr>
2017-01-26 15:48:26 +0100

    Adds references to RFCs in docs

--- mail/mail.go
+++ mail/mail.go
@@ -1,2 +1,4 @@
 // Package mail implements reading and writing mail messages.
+//
+// RFC 5322 defines the Internet Message Format.
 package mail
--- message.go
+++ message.go
@@ -1,2 +1,5 @@
-// Package message implements the Internet Message Format.
+// Package message implements reading and writing multipurpose messages.
+//
+// RFC 2045, RFC 2046 and RFC 2047 defines MIME, and RFC 2183 defines the
+// Content-Disposition header field.
 package message
