621d143c8065e8ccc773c2813acd6193a5f47961
emersion <contact@emersion.fr>
2017-01-26 14:57:50 +0100

    charset: adds unknwon charset test for DecodeHeader

--- charset/header_test.go
+++ charset/header_test.go
@@ -16,3 +16,16 @@ func TestHeader(t *testing.T) {
 		t.Errorf("Expected decoded string to be %q but got %q", s, dec)
 	}
 }
+
+func TestDecodeHeader_unknownCharset(t *testing.T) {
+	enc := "=?idontexist?q?Hey you?="
+
+	dec, err := DecodeHeader(enc)
+
+	if err == nil {
+		t.Error("Expected an error while decoding invalid header")
+	}
+	if dec != enc {
+		t.Errorf("Expected decoded string to fallback to %q but got %q", enc, dec)
+	}
+}
