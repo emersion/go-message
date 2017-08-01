601b4361af84ce7f8c285b036be9133f985ce750
yqylovy <yqylovy@gmail.com>
2017-08-01 16:14:00 +0800

    change gb2312 back to use GBK

--- charset/charset.go
+++ charset/charset.go
@@ -17,7 +17,7 @@ var charsets = map[string]encoding.Encoding{
 	"big5":         traditionalchinese.Big5,
 	"euc-jp":       japanese.EUCJP,
 	"gbk":          simplifiedchinese.GBK,
-	"gb2312":       simplifiedchinese.HZGB2312,
+	"gb2312":       simplifiedchinese.GBK, // as  GBK is a superset of HZGB2312,so just use GBK
 	"iso-2022-jp":  japanese.ISO2022JP,
 	"iso-8859-1":   charmap.ISO8859_1,
 	"iso-8859-2":   charmap.ISO8859_2,
