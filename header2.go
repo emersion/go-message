9f4f2b4b93625abeea7471d775b2d4ca9b78960e
emersion <contact@emersion.fr>
2019-03-23 18:05:49 +0200

    More docs for new Header API

--- header2.go
+++ header2.go
@@ -17,6 +17,14 @@ func newHeaderField(k, v string) headerField {
 }
 
 // A Header represents the key-value pairs in a message header.
+//
+// The header representation is idempotent: if the header can be read and
+// written, the result will be exactly the same as the original (including
+// whitespace). This is required for e.g. DKIM.
+//
+// Mutating the header is restricted: the only two allowed operations are
+// inserting a new header field at the top and deleting a header field. This is
+// again necessary for DKIM.
 type Header2 struct {
 	// Fields are in reverse order so that inserting a new field at the top is
 	// cheap.
