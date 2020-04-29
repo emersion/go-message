c0e958deb09da9ca83fd5e101b2cd69a5211d871
Simon Ser <contact@emersion.fr>
2020-04-29 11:57:41 +0200

    textproto: ensure HeaderFields is implemented in tests

--- textproto/header_test.go
+++ textproto/header_test.go
@@ -9,6 +9,11 @@ import (
 	"testing"
 )
 
+var (
+	_ HeaderFields = (*headerFields)(nil)
+	_ HeaderFields = (*headerFieldsByKey)(nil)
+)
+
 var from = "Mitsuha Miyamizu <mitsuha.miyamizu@example.com>"
 var to = "Taki Tachibana <taki.tachibana@example.org>"
 var received2 = "from example.com by example.org"
