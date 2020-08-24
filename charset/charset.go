emersion/go-message

commit d053f95466fb9f3691e96cc6ddd3176424627d29

charset: remove charsets supported by htmlindex
These aren't necessary anymore since 18756f72b12f ("charset: Try htmlindex,
too").

charset/charset.go
@@ -34,13 +34,9 @@ var charsets = map[string]encoding.Encoding{
 	"cp367":            encoding.Nop,
 	"ascii":            encoding.Nop, // non-standard
 
-	"ansi_x3.110-1983": charmap.ISO8859_1,     // see RFC 1345 page 62, mostly superset of ISO 8859-1
-	"gb2312":           simplifiedchinese.GBK, // GBK is a superset of HZGB2312
+	"ansi_x3.110-1983": charmap.ISO8859_1, // see RFC 1345 page 62, mostly superset of ISO 8859-1
 	// disabled due to https://github.com/emersion/go-message/issues/95
 	"hz-gb-2312": nil,
-	"cp1250":     charmap.Windows1250,
-	"cp1251":     charmap.Windows1251,
-	"cp1252":     charmap.Windows1252,
 }
 
 func init() {

commit 4500fbb11f59cf8d6639e8d1a4cda5b245598ed9

charset: fix htmlindex fallback
In some cases, ianaindex would return a nil Encoding to mean that it
doesn't support the charset *and* a nil error to mean that the supplied
charset is a valid IANA charset. This isn't great, and [1] contains an
idea to fix this bad API.

In the meantime, check enc == nil instead err != nil.

[1]: https://go-review.googlesource.com/c/text/+/212077/5#message-59f8d7185d7f10e825a79541046b0aff668d7494

Fixes: d053f95466fb ("charset: remove charsets supported by htmlindex")
Closes: https://github.com/emersion/go-message/issues/104

charset/charset.go
@@ -14,7 +14,6 @@ import (
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/encoding/htmlindex"
 	"golang.org/x/text/encoding/ianaindex"
-	"golang.org/x/text/encoding/simplifiedchinese"
 )
 
 // Quirks table for charsets not handled by ianaindex
@@ -47,13 +46,15 @@ func init() {
 func Reader(charset string, input io.Reader) (io.Reader, error) {
 	var err error
 	enc, ok := charsets[strings.ToLower(charset)]
-	if !ok {
+	if ok && enc == nil {
+		return nil, fmt.Errorf("charset %q: charset is disabled", charset)
+	} else if !ok {
 		enc, err = ianaindex.MIME.Encoding(charset)
 	}
-	if err != nil {
+	if enc == nil {
 		enc, err = ianaindex.MIME.Encoding("cs" + charset)
 	}
-	if err != nil {
+	if enc == nil {
 		enc, err = htmlindex.Get(charset)
 	}
 	if err != nil {

charset/charset_test.go
@@ -84,7 +84,7 @@ func TestDisabledCharsetReader(t *testing.T) {
 		t.Errorf("%v encoding is disabled and should give an error", "hz-gb-2312")
 		return
 	}
-	if !strings.HasSuffix(err.Error(), "unsupported charset") {
+	if !strings.HasSuffix(err.Error(), "charset is disabled") {
 		t.Errorf("expected error to end in 'unsupported charset', got %v",
 			err.Error())
 	}