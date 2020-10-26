emersion/go-message

commit bb063b88a64b3387b413be92c195c3339d715860

charset: drop us-ascii quirk
This is now handled in x/text [1].

[1]: https://github.com/golang/text/commit/a8b4671254579a87fadf9f7fa577dc7368e9d009

charset/charset.go
@@ -21,8 +21,6 @@ import (
 // For aliases, see
 // https://www.iana.org/assignments/character-sets/character-sets.xhtml
 var charsets = map[string]encoding.Encoding{
-	// us-ascii not handled by ianaindex
-	"us-ascii":         encoding.Nop,
 	"iso-ir-6":         encoding.Nop,
 	"ansi_x3.4-1968":   encoding.Nop,
 	"ansi_x3.4-1986":   encoding.Nop,

commit eb8e213ccf1bb7ff45583dd12a8858ab13d80377

charset: drop more us-ascii aliases quirks
All of these are now covered by ianaindex.

Also update x/text to the correct commit, forgot about that.

Fixes: bb063b88a64b ("charset: drop us-ascii quirk")

charset/charset.go
@@ -21,16 +21,6 @@ import (
 // For aliases, see
 // https://www.iana.org/assignments/character-sets/character-sets.xhtml
 var charsets = map[string]encoding.Encoding{
-	"iso-ir-6":         encoding.Nop,
-	"ansi_x3.4-1968":   encoding.Nop,
-	"ansi_x3.4-1986":   encoding.Nop,
-	"iso_646.irv:1991": encoding.Nop,
-	"iso646-us":        encoding.Nop,
-	"us":               encoding.Nop,
-	"ibm367":           encoding.Nop,
-	"cp367":            encoding.Nop,
-	"ascii":            encoding.Nop, // non-standard
-
 	"ansi_x3.110-1983": charmap.ISO8859_1, // see RFC 1345 page 62, mostly superset of ISO 8859-1
 	// disabled due to https://github.com/emersion/go-message/issues/95
 	"hz-gb-2312": nil,

go.mod
@@ -7,5 +7,5 @@ require (
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594
 	github.com/martinlindhe/base36 v1.1.0
 	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/text v0.3.3
+	golang.org/x/text v0.3.4-0.20201021145329-22f1617af38e
 )

go.sum
@@ -13,4 +13,6 @@ github.com/stretchr/testify v1.3.0 h1:TivCn/peBQ7UY8ooIcPgZFpTNSz0Q2U6UrFlUfqbe0
 github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
 golang.org/x/text v0.3.3 h1:cokOdA+Jmi5PJGXLlLllQSgYigAEfHXJAERHVMaCc2k=
 golang.org/x/text v0.3.3/go.mod h1:5Zoc/QRtKVWzQhOtBMvqHzDpF6irO9z98xDceosuGiQ=
+golang.org/x/text v0.3.4-0.20201021145329-22f1617af38e h1:0kyKOEC0chG7FKmnf/1uNwvDLc3NtNTRip2rXAN9nwI=
+golang.org/x/text v0.3.4-0.20201021145329-22f1617af38e/go.mod h1:5Zoc/QRtKVWzQhOtBMvqHzDpF6irO9z98xDceosuGiQ=
 golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=