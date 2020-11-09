61b8de525812b1c66f039930b764799c297ce660
Reto Brunner <reto@labrat.space>
2020-11-09 23:02:41 +0100

    Export ParseAddressList and add ParseAddress for symetry
    This exposes essentially the same API as net/mail for parsing strings.
    Doc strings should make it obvious that this should not be used on a header.

--- mail/address.go
+++ mail/address.go
@@ -13,17 +13,30 @@ import (
 // Address is expected
 type Address = mail.Address
 
-func parseAddressList(s string) ([]*Address, error) {
-	parser := mail.AddressParser{
-		&mime.WordDecoder{message.CharsetReader},
-	}
-	return parser.ParseList(s)
-}
-
 func formatAddressList(l []*Address) string {
 	formatted := make([]string, len(l))
 	for i, a := range l {
 		formatted[i] = a.String()
 	}
 	return strings.Join(formatted, ", ")
+}
+
+// ParseAddress parses a single RFC 5322 address, e.g. "Barry Gibbs <bg@example.com>"
+// Use this function only if you parse from a string, if you have a Header use
+// Header.AddressList instead
+func ParseAddress(address string) (*Address, error) {
+	parser := mail.AddressParser{
+		&mime.WordDecoder{message.CharsetReader},
+	}
+	return parser.Parse(address)
+}
+
+// ParseAddressList parses the given string as a list of addresses.
+// Use this function only if you parse from a string, if you have a Header use
+// Header.AddressList instead
+func ParseAddressList(list string) ([]*Address, error) {
+	parser := mail.AddressParser{
+		&mime.WordDecoder{message.CharsetReader},
+	}
+	return parser.ParseList(list)
 }
